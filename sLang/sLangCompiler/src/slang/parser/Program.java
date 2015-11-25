bdb318a92b4b6082230a171df298454f7231fe3f
HobbyProgrammer <tohu@online.de>
2015-11-25 20:04:06 +0100

    Added Program class
    the interpreter will get one instance of this class, which contains all
    information needed for compilation.

---
+++ sLang/sLangCompiler/src/slang/parser/Program.java
@@ -0,0 +1,23 @@
+package slang.parser;
+
+import slang.parser.Function;
+import slang.parser.statements.VariableDeclaration;
+import slang.parser.statements.expressionstats.Assignment;
+
+public class Program
+{
+	public VariableDeclaration[] getVariableDeclarations()
+	{
+		return null;
+	}
+	
+	public Assignment[] getAssignments()
+	{
+		return null;
+	}
+	
+	public Function[] getFunctions()
+	{
+		return null;
+	}
+}
